1629e43af076e7abb411ffbbd4df20a6a241da96
Jakob Getz <jakob@getz.de>
2021-08-09 18:46:13 +0200

    setup tests

--- tests/createComponent.test.ts
+++ tests/createComponent.test.ts
@@ -1,22 +1,19 @@
-// import path from "path";
-const path = require("path");
-// import fs from "fs";
-const fs = require("fs");
-// import child_process from "child_process";
-const child_process = require("child_process");
+import path from "path";
+import fs from "fs";
+import child_process from "child_process";
 import { testConfig } from "./testConfig";
 
 describe("test createComponent", () => {
   it("create Folder on top level", () => {
     const componentName = "Component";
-    const location = ".";
+    const location = "./tests/gen";
     const fileExtension = "ts";
     const fullPath = path.join(location, componentName);
     if (fs.existsSync(fullPath) && fs.lstatSync(fullPath).isDirectory()) {
       fs.rmdirSync(fullPath, { recursive: true });
     }
     child_process.execSync(
-      `node-ts ${testConfig.scriptLocation}createComponent.ts ${componentName} ${location}`
+      `node ${testConfig.scriptLocation}createComponent.js ${componentName} ${location}`
     );
 
     expect(fs.existsSync(fullPath)).toBe(true);
@@ -24,6 +21,6 @@ describe("test createComponent", () => {
       fs.existsSync(`${fullPath}/${componentName}.${fileExtension}x`)
     ).toBe(true);
     expect(fs.existsSync(`${fullPath}/styles.${fileExtension}`)).toBe(true);
-    expect(fs.existsSync(`${fullPath}/index${fileExtension}`)).toBe(true);
+    expect(fs.existsSync(`${fullPath}/index.${fileExtension}`)).toBe(true);
   });
 });
--- tests/testConfig.ts
+++ tests/testConfig.ts
@@ -1,3 +1,3 @@
 export const testConfig = {
-    scriptLocation: "../"
+    scriptLocation: "./dist/"
 }
